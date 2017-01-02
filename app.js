f4399a9797d5de8e58144aa397b9a335374fbec7
Jaison Peres <jaisoncperes@gmail.com>
2017-01-02 14:31:59 -0200

    log port

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var app = require('./config/server');
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function(){
-  console.log('Servidor Inicializado');
+  console.log('Servidor Inicializado na porta: '+app.get('port'));
 });
