bf870adb761b25c477c76e5f8d5dbb3d3e1d16c5
Jaison Peres <jaisoncperes@gmail.com>
2017-01-02 14:50:48 -0200

    - dirname

--- app/routes/signin.js
+++ app/routes/signin.js
@@ -1,9 +1,8 @@
 module.exports = function(app){
-  app.get(__dirname+'/login', function(req, res){
-    //app.app.controllers.signin.sign(app, req, res);
-    res.send('até aqui ok');
+  app.get('/login', function(req, res){
+    app.app.controllers.signin.sign(app, req, res);
   });
-  app.post(__dirname+'/login', function(req, res){
+  app.post('/login', function(req, res){
     app.app.controllers.signin.signin(app, req, res);
   });
 }
