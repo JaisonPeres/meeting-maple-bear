6b3a675d410b3b9f8150ffc08b017165a77188fd
Jaison Peres <jaisoncperes@gmail.com>
2017-01-02 14:45:10 -0200

    dirname + login

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = function(app){
-  app.get(__dirname+'/', function(req, res){;//HOME
+  app.get(__dirname+'/', function(req, res){;
     app.app.controllers.home.index(app, req, res);
     //res.send('Home, até aqui tudo bem');
   });
--- app/routes/signin.js
+++ app/routes/signin.js
@@ -1,8 +1,8 @@
 module.exports = function(app){
-  app.get('/login', function(req, res){
+  app.get(__dirname+'/login', function(req, res){
     app.app.controllers.signin.sign(app, req, res);
   });
-  app.post('/login', function(req, res){
+  app.post(__dirname+'/login', function(req, res){
     app.app.controllers.signin.signin(app, req, res);
   });
 }
