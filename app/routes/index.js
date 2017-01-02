4398e64b0ce93796ee138f23429d2b21774d953b
Jaison Peres <jaisoncperes@gmail.com>
2017-01-02 14:39:14 -0200

    test rota index 3

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = function(application){
-  application.get('/', function(req, res){;//HOME
+  application.get(__dirname+'/', function(req, res){;//HOME
     application.app.controllers.home.index(app, req, res);
     //res.send('Home, até aqui tudo bem');
   });
